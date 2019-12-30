75287df3750984e1cf06739766099e06fc29357c
fly1057 <fly1057@yeah.net>
2019-12-30 12:03:08 +0800

    master

--- GUItest.py
+++ GUItest.py
@@ -4,4 +4,19 @@ from matplotlib.backends.backend_qt5agg import (FigureCanvasAgg as
                                                 FigureCanvas,
                                                 NavigationToolbar2QT as
                                                 NavigationToolbar)
-Ui_MainWindow , QMainWindow = loadUiType("kongzai.ui") 
+Ui_MainWindow, QMainWindow = loadUiType("kongzaiQMainWindow.ui")
+
+
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self, ):
+        super(Main, self).__init__()
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    import sys
+    from PyQt5 import QtGui
+    app = QtGui.QGuiApplication(sys.argv)
+    main = Main()
+    main.show()
+    sys.exit(app.exec_())
