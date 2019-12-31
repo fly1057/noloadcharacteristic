0f1052f3d491f8139a9302977985dc7af15b77b7
fly1057 <fly1057@yeah.net>
2020-01-01 04:26:47 +0800

    master

--- GUItest copy.py
+++ GUItest copy.py
@@ -7,7 +7,7 @@ import pandas as pd
 from Ui_kongzaiQMainWindow import Ui_MainWindow
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (
-    FigureCanvasQTAgg as  #不是FigureCanvasAgg，pyqt5已经更新了
+    FigureCanvasQTAgg as  # 不是FigureCanvasAgg，pyqt5已经更新了
     FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
 from scipy.optimize import least_squares, leastsq
@@ -21,7 +21,7 @@ class Main(QtWidgets.QMainWindow):
         # 在这里引入matplotlib的代码结构，首先建立figure，然后建立canvas，其中canvas是子类
         # 然后将canvas加入到之前在Qt designer中建立的layout中去，关键是要在设计时留一个空出来
 
-        #控件与函数的连接  self+ui容器+控件+动作+connect（函数名称）
+        # 控件与函数的连接  self+ui容器+控件+动作+connect（函数名称）
         self.ui.pushButton_NoLoadCalculateAutoCalculate.clicked.connect(
             self.NoLoadCalculateAutoCalculate)
         self.ui.pushButton_NoLoadCalculateReadCSV.clicked.connect(
@@ -29,7 +29,6 @@ class Main(QtWidgets.QMainWindow):
         self.ui.pushButton_Reset.clicked.connect(self.Reset)
 
         self.Reset()
-        self.ShowResultsText()
 
     def NoLoadCalculateReadCSV(self):
         try:
@@ -53,14 +52,13 @@ class Main(QtWidgets.QMainWindow):
 
     def Reset(self):
         try:
-            #这里使用了一个小技巧，即先删除一下然后再增加一个图，
-            #如果报异常那么就转到异常处理代码，不删仅增
+            # 这里使用了一个小技巧，即先删除一下然后再增加一个图，
+            # 如果报异常那么就转到异常处理代码，不删仅增
             self.rmmpl()
             fig1 = Figure(tight_layout=True)
             ax1f1 = fig1.add_subplot(111)
             ax1f1.pcolormesh(np.random.rand(10, 10))
             self.addmpl(fig1)
-            self.ExciterTransferCalculateSTN = float(self.ui.lineEdit_ExciterTransferCalculateSTN.text())
 
         except Exception as e:
             print(e)
@@ -73,12 +71,12 @@ class Main(QtWidgets.QMainWindow):
 
     def ShowPlot(self):
         try:
-            self.rmmpl()  #这句话必须要有，不然canvas会一直增加
+            self.rmmpl()  # 这句话必须要有，不然canvas会一直增加
             fig1 = Figure(tight_layout=True)
-            #由于未采用pyplot而采用的figure，那么实际上ax1f1是一个包含axes的汇总的集合
-            #这个可以在debug窗口看出来，那么可以ax1f1.axes来使用axes的所有属性，如果有
-            #些出入的话，比如plt.axes.xlabel可以用ax1f1.axes.set_xlabel来实现，总之可以在
-            #debug窗口确认这个ax1f1是怎样的class，然后猜大概的属性
+            # 由于未采用pyplot而采用的figure，那么实际上ax1f1是一个包含axes的汇总的集合
+            # 这个可以在debug窗口看出来，那么可以ax1f1.axes来使用axes的所有属性，如果有
+            # 些出入的话，比如plt.axes.xlabel可以用ax1f1.axes.set_xlabel来实现，总之可以在
+            # debug窗口确认这个ax1f1是怎样的class，然后猜大概的属性
             ax1f1 = fig1.add_subplot(111)
 
             # 所画曲线
@@ -167,19 +165,19 @@ class Main(QtWidgets.QMainWindow):
 
             ax1f1.text(self.IFD_air_100_value / 2,
                        1,
-                       'IFDB=' + str(round(self.IFDB[0], 1)) + "A",
+                       'IFDB=' + str(round(self.IFDB, 1)) + "A",
                        horizontalalignment='center',
                        verticalalignment='center')
 
             ax1f1.text(self.IFD_air_100_value / 2,
                        0.9,
-                       'UFDB=' + str(round(self.UFDB[0], 1)) + "V",
+                       'UFDB=' + str(round(self.UFDB, 1)) + "V",
                        horizontalalignment='center',
                        verticalalignment='center')
 
             ax1f1.text(self.IFD_air_100_value / 2,
                        0.8,
-                       'KFD=' + str(round(self.KFD[0], 2)),
+                       'KFD=' + str(round(self.KFD, 2)),
                        horizontalalignment='center',
                        verticalalignment='center')
 
@@ -202,9 +200,9 @@ class Main(QtWidgets.QMainWindow):
     def UpdateTableWidgetFromDataFrame(self):
         try:
             print("begin UpdateTableWidgetFromDataFrame")
-            #动态改变tablewidget的行数，保证行数是动态改变，不再要求上升阶段的点数是固定的
-            #可以先在qt designer里面改一下，然后借鉴里面的写法，和利用Excel的宏一样
-            #df.shape是一个tuple，第一个参数是行数，第二个参数是列数
+            # 动态改变tablewidget的行数，保证行数是动态改变，不再要求上升阶段的点数是固定的
+            # 可以先在qt designer里面改一下，然后借鉴里面的写法，和利用Excel的宏一样
+            # df.shape是一个tuple，第一个参数是行数，第二个参数是列数
             self.ui.tableWidget_RawData.setRowCount(self.df.shape[0])
             self.ui.tableWidget_RawData.setColumnCount(self.df.shape[1])
 
@@ -223,8 +221,25 @@ class Main(QtWidgets.QMainWindow):
     def UpdateSelfFromPanel(self):
         try:
             print("begin UpdateSelfFromPanel")
-            self.ExciterTransferCalculateSTN = float(self.ui.lineEdit_ExciterTransferCalculateSTN.text)
-            print(self.ExciterTransferCalculateSTN)
+            self.LinearScope = float(self.ui.lineEdit_LinearScope.text())
+            self.STN = float(self.ui.lineEdit_STN.text())*1000
+            self.Uk = float(self.ui.lineEdit_Uk.text())
+            self.ULN = float(self.ui.lineEdit_ULN.text())
+
+            self.Rising1Ut = float(self.ui.lineEdit_Rising1Ut.text())
+            self.Rising1Ufd = float(self.ui.lineEdit_Rising1Ufd.text())
+            self.Rising1Ifd = float(self.ui.lineEdit_Rising1Ifd.text())
+            self.Rising2Ut = float(self.ui.lineEdit_Rising2Ut.text())
+            self.Rising2Ufd = float(self.ui.lineEdit_Rising2Ufd.text())
+            self.Rising2Ifd = float(self.ui.lineEdit_Rising2Ifd.text())
+
+            self.Falling1Ut = float(self.ui.lineEdit_Falling1Ut.text())
+            self.Falling1Ufd = float(self.ui.lineEdit_Falling1Ufd.text())
+            self.Falling1Ifd = float(self.ui.lineEdit_Falling1Ifd.text())
+            self.Falling2Ut = float(self.ui.lineEdit_Falling2Ut.text())
+            self.Falling2Ufd = float(self.ui.lineEdit_Falling2Ufd.text())
+            self.Falling2Ifd = float(self.ui.lineEdit_Falling2Ifd.text())
+
             print("end UpdateSelfFromPanel")
         except Exception as e:
             print(e)
@@ -234,18 +249,24 @@ class Main(QtWidgets.QMainWindow):
     def UpdatePanelFromSelf(self):
         try:
             print("begin UpdatePanelFromSelf")
-            self.ui.lineEdit_ExciterTransferCalculateXcReal.setText(str(self.XcReal))
-            self.ui.lineEdit_ExciterTransferCalculateXcpu.setText(str(self.Xcpu))
-            self.ui.lineEdit_LimiterCalculateIFDB.setText(str(self.IFDB[0]))
-            self.ui.lineEdit_LimiterCalculateUFDB.setText(str(self.UFDB[0]))
-            self.ui.lineEdit_LimiterCalculateKFD.setText(str(self.KFD[0]))
-            # self.ui.lineEdit_LimiterCalculateUmax.setText(str(self.Umax))
-            # self.ui.lineEdit_LimiterCalculateUmin.setText(str(self.Umin))
-            self.ui.lineEdit_LimiterCalculatea.setText(str(self.a))
-            self.ui.lineEdit_LimiterCalculateb.setText(str(self.b))
-            self.ui.lineEdit_LimiterCalculaten.setText(str(self.n))
-            self.ui.lineEdit_LimiterCalculateSG100.setText(str(self.SG100))
-            self.ui.lineEdit_LimiterCalculateSG120.setText(str(self.SG120))
+            self.ui.lineEdit_XcReal.setText(str(self.XcReal))
+            self.ui.lineEdit_Xcpu.setText(str(self.Xcpu))
+            self.ui.lineEdit_Rising1Angle.setText(str(self.Rising1Angle))
+            self.ui.lineEdit_Rising2Angle.setText(str(self.Rising2Angle))
+            self.ui.lineEdit_Falling1Angle.setText(str(self.Falling1Angle))
+            self.ui.lineEdit_Falling2Angle.setText(str(self.Falling2Angle))
+            self.ui.lineEdit_AngleAVGmax.setText(str(self.AngleAVGmax))
+            self.ui.lineEdit_AngleAVGmin.setText(str(self.AngleAVGmin))           
+            self.ui.lineEdit_IFDB.setText(str(self.IFDB))
+            self.ui.lineEdit_UFDB.setText(str(self.UFDB))
+            self.ui.lineEdit_KFD.setText(str(self.KFD))
+            self.ui.lineEdit_Umax.setText(str(self.Umax))
+            self.ui.lineEdit_Umin.setText(str(self.Umin))
+            self.ui.lineEdit_a.setText(str(self.a))
+            self.ui.lineEdit_b.setText(str(self.b))
+            self.ui.lineEdit_n.setText(str(self.n))
+            self.ui.lineEdit_SG100.setText(str(self.SG100))
+            self.ui.lineEdit_SG120.setText(str(self.SG120))
 
             print("end UpdatePanelFromSelf")
         except Exception as e:
@@ -255,7 +276,24 @@ class Main(QtWidgets.QMainWindow):
 
     def ShowResultsText(self):
         try:
-            self.ui.textBrowser.setText("hello world")
+            self.ui.textBrowser.setText("IFDB=" + str(round(self.IFDB,4)) +"\n" \
+                                        "UFDB=" + str(round(self.UFDB,4)) + "\n" \
+                                        "KFD=" + str(round(self.KFD,4)) + "\n" \
+                                        "a=" + str(self.a) + "\n" \
+                                        "b=" + str(round(self.b,4)) + "\n" \
+                                        "n=" + str(round(self.n,4)) + "\n" \
+                                        "SG1.0=" + str(round(self.SG100,4)) + "\n" \
+                                        "SG1.2=" + str(round(self.SG120, 4)) + "\n"
+                                        "Rising1Angle=" + str(round(self.Rising1Angle, 4)) + "\n" \
+                                        "Rising2Angle=" + str(round(self.Rising2Angle, 4)) + "\n" \
+                                        "Falling1Angle=" + str(round(self.Falling1Angle, 4)) + "\n" \
+                                        "Falling2Angle=" + str(round(self.Falling2Angle, 4)) + "\n" \
+                                        "αmin=" + str(round(self.AngleAVGmin, 4)) + "\n" \
+                                        "αmax=" + str(round(self.AngleAVGmax, 4)) + "\n" \
+                                        "Umax=" + str(round(self.Umax, 4)) + "\n"\
+                                        "Umin=" + str(round(self.Umin, 4)) + "\n"\
+                                        )
+
         except Exception as e:
             print(e)
             print(e.__traceback__.tb_frame.f_globals["__file__"])  # 发生异常所在的文件
@@ -288,12 +326,13 @@ class Main(QtWidgets.QMainWindow):
 
     def NoLoadCalculateAutoCalculate(self):
         try:
-
-            self.df = self.df.rename(columns={
-                u'UAB(p.u.)': u'UAB',
-                u'UFD(V)': u'UFD',
-                u'IFD(A)': u'IFD'
-            })
+            self.UpdateSelfFromPanel()
+            # self.df = self.df.rename(columns={
+            #     u'UAB(p.u.)': u'UAB',
+            #     u'UFD(V)': u'UFD',
+            #     u'IFD(A)': u'IFD'
+            # })
+            self.df.columns = ['UAB', 'UFD', 'IFD']
             # 截取上升段的数据，注意需要找到最大值index后加1
             self.UAB = self.df.UAB[0:(self.df.UAB.idxmax() + 1)]
             self.IFD = self.df.IFD[0:(self.df.UAB.idxmax() + 1)]
@@ -437,8 +476,8 @@ class Main(QtWidgets.QMainWindow):
             self.n = 1 + np.log(self.SG120 / self.SG100) / np.log(1.2)
 
             ###############################################################################
-            #静态放大倍数计算
-            #通过abn先计算SG
+            # 静态放大倍数计算
+            # 通过abn先计算SG
             def SG_function(UAB, a, b, n):
                 SG = b / a * UAB**(n - 1)
                 return SG
@@ -447,42 +486,58 @@ class Main(QtWidgets.QMainWindow):
                                   self.n)
             print("SG = ", self.SG)
 
-            self.IFDB = []
-            self.UFDB = []
-            self.KFD = []
-            self.ULN = 450  # V
-            self.STN = 10**6  # W
-            self.Uk = 0.06
-            self.XcReal = self.ULN**2 / self.STN * self.Uk
+            self.IFDBseq = []
+            self.UFDBseq = []
+            self.KFDseq = []
+            self.XcReal = self.ULN**2 /self.STN * self.Uk
 
             for i in np.arange(self.IFD_saturation_sq.__len__()):
                 if i == 0:
                     pass
                 else:
-                    self.IFDB.append(
+                    self.IFDBseq.append(
                         (self.IFD_saturation_sq[i] /
                          (1 + self.SG[i])) / self.UAB_saturation_sq[i])
-                    self.UFDB.append(
+                    self.UFDBseq.append(
                         (self.UFD_saturation_sq[i] /
                          (1 + self.SG[i])) / self.UAB_saturation_sq[i])
-                    self.KFD.append(
+                    self.KFDseq.append(
                         1.35 * self.ULN * self.UAB_saturation_sq[i] /
                         ((self.UFD_saturation_sq[i] + self.IFD_saturation_sq[i]
-                          *self.XcReal) /
+                          * self.XcReal) /
                          (1 + self.SG[i])))
 
-            print("IFDB = ", self.IFDB)
-            print("UFDB = ", self.UFDB)
-            print("KFD = ", self.KFD)
+            print("IFDB = ", self.IFDBseq)
+            print("UFDB = ", self.UFDBseq)
+            print("KFD = ", self.KFDseq)
 
-            self.Xcpu = self.XcReal / (self.UFDB[0] / self.IFDB[0])
-            self.ControlAngleCalculateRisingFirstPointAngle = 1
+            self.UFDB = self.UFDBseq[0]
+            self.IFDB = self.IFDBseq[0]
+            self.KFD = self.KFDseq[0]
+
+            self.Rising1Angle = 180/np.pi*np.arccos((self.Rising1Ufd + self.Rising1Ifd * self.XcReal)/(1.35*self.ULN*self.Rising1Ut ))
+            self.Rising2Angle = 180/np.pi*np.arccos((self.Rising2Ufd + self.Rising2Ifd * self.XcReal)/(1.35*self.ULN*self.Rising2Ut ))
+
+            self.Falling1Angle = 180/np.pi*np.arccos((self.Falling1Ufd + self.Falling1Ifd * self.XcReal)/(1.35*self.ULN*self.Falling1Ut ))
+            self.Falling2Angle = 180/np.pi*np.arccos((self.Falling2Ufd + self.Falling2Ifd * self.XcReal)/(1.35*self.ULN*self.Falling2Ut ))
+            
+            self.AngleAVGmin = np.average(
+                [self.Rising1Angle, self.Rising2Angle])
+            self.AngleAVGmax = np.average(
+                [self.Falling1Angle, self.Falling2Angle])
+
+            self.Umax = 1.35*self.ULN * \
+                np.cos(self.AngleAVGmin/180*np.pi) / self.UFDB
+            self.Umin = 1.35*self.ULN * \
+                np.cos(self.AngleAVGmax/180*np.pi) / self.UFDB
+
+            self.Xcpu = self.XcReal / (self.UFDB / self.IFDB)
+
             ###############################################################################
 
             self.ShowPlot()
-            self.ShowResultsText()
             self.UpdatePanelFromSelf()
-            self.UpdateSelfFromPanel()
+            self.ShowResultsText()
 
         except Exception as e:
             print(e)
@@ -492,9 +547,9 @@ class Main(QtWidgets.QMainWindow):
 
 if __name__ == "__main__":
     import sys
-    QtWidgets.QApplication.setAttribute(Qt.AA_EnableHighDpiScaling) 
+    QtWidgets.QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = Main()
     MainWindow.show()
-    #MainWindow.showMaximized()
+    # MainWindow.showMaximized()
     sys.exit(app.exec_())
